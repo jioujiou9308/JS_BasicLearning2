f99186b528c653cce3d807a6eb550b50d8f4ba76
Chen-Wei <p1789632p@gmail.com>
2023-01-04 16:35:35 +0800

    0104update

--- Locastorage/localstorage.js
+++ Locastorage/localstorage.js
@@ -7,7 +7,7 @@ let players = {
 	player3: 'peter2',
 };
 
-console.log(players);
+// console.log(players);
 //要將物件儲存在localstorage一定要先將其變成json格式
 localStorage.setItem('playerList2', JSON.stringify(players));
 
@@ -17,8 +17,8 @@ let fruits = {
 };
 console.log(typeof fruits); //object
 let jsonFruit = JSON.stringify(fruits);
-console.log(typeof jsonFruit); // string
-console.log(jsonFruit); //已經都轉換成字串格式了
+// console.log(typeof jsonFruit); // string
+// console.log(jsonFruit); //已經都轉換成字串格式了
 localStorage.setItem('fruitList', jsonFruit);
 
 //如果只有指提取資料並沒有轉換成原來的格式localStorage.getItem("fruitList")，那還是一樣字串
--- test.js
+++
@@ -1,7 +0,0 @@
-const data = [4,7,9,10,15]
-
-const newData = data.filter(item => item > 5 && item<12)
-console.log(newData);
-
-const data2 = data.map(item => item +5)
-console.table(data2);
